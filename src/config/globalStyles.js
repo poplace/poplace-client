377592aaeb3933c238b7a0802a38ea8c6c8bfbd8
Onea <onea8906@gmail.com>
2021-09-10 12:28:00 +0900

    chore: add global styles in config

---
+++ src/config/globalStyles.js
@@ -0,0 +1,11 @@
+import { Dimensions } from "react-native";
+
+export const { width, height } = Dimensions.get("screen");
+
+export const color = {
+  poplaceDarkColor: "#453536",
+  poplacelightColor: "#766162",
+  poplaceRedColor: "#f78582",
+  poplaceWhiteColor: "#ffffff",
+  poplaceGrayColor: "gray",
+};

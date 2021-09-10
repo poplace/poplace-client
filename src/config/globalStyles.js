109bd3b09d3ac6702d1a103da32973e6010a2536
ham-myeongyeon <hmy751@gmail.com>
2021-09-10 22:31:41 +0900

    feat: add globalStyle

---
+++ src/config/globalStyles.js
@@ -0,0 +1,14 @@
+import { Dimensions } from "react-native";
+
+export const { width, height } = Dimensions.get("screen");
+
+const guideScale = Math.sqrt(width * height);
+const scale = Math.sqrt(width * height) / guideScale;
+const horiPer = width / width;
+const vertiPer = height / height;
+
+const verticalScale = (size) => horiPer * (size * height / 700);
+const horizontalScale = (size) => vertiPer * (size * height / 700);
+const moderateScale = (size) => scale * (size * height / 700);
+
+export { moderateScale, verticalScale, horizontalScale };
--- src/screens/DetailPin.js
+++ src/screens/DetailPin.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import { View, Text, StyleSheet, Image, Button } from "react-native";
+import React, { useEffect } from 'react'
+import { View, Text, StyleSheet, Image, Button, TouchableOpacity } from "react-native";
 
 export default function DetailPin() {
+  useEffect(() => {
+
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Image style={styles.image}
+      <Image
+        style={styles.image}
         source={{
           uri: 'https://reactnative.dev/img/tiny_logo.png',
         }}
@@ -13,10 +18,12 @@ export default function DetailPin() {
         <View style={styles.time}>
           <Text>남은시간: 20시간 12분</Text>
         </View>
-        <Button title="저장하기" />
+        <TouchableOpacity text="저장하기" style={styles.button}>
+          <Text style={styles.buttonText}>저장하기</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.tagContainer}></View>
-        <Text style={styles.tag}>#노을이예뻐</Text>
+      <Text style={styles.tag}>#노을이예뻐</Text>
       <View style={styles.textContainer}>
         <Text>dd</Text>
       </View>
@@ -40,19 +47,30 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-evenly",
-    width: "100%",
-    borderWidth: 1,
+    width: "80%",
+    borderColor: 
+    borderBottomWidth: 1,
   },
   time: {
     justifyContent: "center",
   },
+  button: {
+    backgroundColor: "#f78582",
+    borderRadius: 15,
+    marginHorizontal: 5,
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+  },
+  buttonText: {
+    color: "#ffffff",
+  },
   textContainer: {
     flex: 0.8,
-    width: "100%",
-    borderWidth: 1,
+    width: "80%",
   },
   tagContainer: {
     flexWrap: "wrap",
+    marginTop: 10,
     // justifyContent: "center",
     // alignItems: "center",
   },

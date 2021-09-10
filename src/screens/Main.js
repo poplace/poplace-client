795ba8943b7242aebf48bb795701d5e99cd1a262
ham-myeongyeon <hmy751@gmail.com>
2021-09-10 16:37:44 +0900

    refactor: edit unused code

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,12 +1,7 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import { Text } from "react-native";
-import { selectUser } from "../features/userSlice";
 
 export default function Main({ navigation }) {
-  const status = useSelector(state => state.user.status);
-  const userInfo = useSelector(selectUser);
-
   return (
     <Text>this is Main Page</Text>
   );

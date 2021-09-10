ea6ee18ccdf391fa6f7518e503f39ce5a7e6e959
Heesu <phis3210@gmail.com>
2021-09-10 22:55:07 +0900

    chore: remove the annotation

--- src/utils/loginWithGoogle.js
+++ src/utils/loginWithGoogle.js
@@ -4,7 +4,7 @@ import { IOS_CLIENT_ID, ANDROID_CLIENT_ID } from "@env";
 export default async function loginWithGoogle() {
   try {
     const result = await Google.logInAsync({
-      // iosClientId: IOS_CLIENT_ID,
+      iosClientId: IOS_CLIENT_ID,
       androidClientId: ANDROID_CLIENT_ID,
       scopes: ["profile", "email"],
     });
